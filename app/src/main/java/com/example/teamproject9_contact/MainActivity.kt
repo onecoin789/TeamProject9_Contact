ffc773976034b1bbaf34fd8ff15731e0ce64b173
onecoin789 <one_coin@naver.com>
2024-04-22 15:27:43 +0900

    first commit

---
+++ app/src/main/java/com/example/teamproject9_contact/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.teamproject9_contact
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.teamproject9_contact.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+
+    }
+}
