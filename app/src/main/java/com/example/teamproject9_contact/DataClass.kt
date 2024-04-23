75f5b47f4ffdcd44242c223914354fe71864ef3f
999115 <999n115@gmail.com>
2024-04-23 13:08:44 +0900

    feat: 추가 멀티뷰타입 리사이클러뷰
    기본 기능 구현 완료

---
+++ app/src/main/java/com/example/teamproject9_contact/DataClass.kt
@@ -0,0 +1,4 @@
+package com.example.teamproject9_contact
+
+class DataClass {
+}
--- app/src/main/java/com/example/teamproject9_contact/MainActivity.kt
+++ app/src/main/java/com/example/teamproject9_contact/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.teamproject9_contact
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.example.teamproject9_contact.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentDataListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -14,5 +16,20 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        setFragment1(ContactListFragment())
+
     }
+
+    override fun onDataReceived(infoData: DataClass) {
+        TODO("Not yet implemented")
+    }
+
+    private fun setFragment1(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.layout_fragment, fragment)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
+    }
+
 }
