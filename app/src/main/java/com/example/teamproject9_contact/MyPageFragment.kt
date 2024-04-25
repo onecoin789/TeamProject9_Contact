ac636fad59a966f9787c823878731b9ca599db90
onecoin <one_coin@naver.com>
2024-04-25 20:51:07 +0900

    add MyPageDialog 추가, MyPageFragment에 데이터 전달, DetailPage 메인버튼 제거

--- app/src/main/java/com/example/teamproject9_contact/MyPageFragment.kt
+++ app/src/main/java/com/example/teamproject9_contact/MyPageFragment.kt
@@ -3,15 +3,21 @@ package com.example.teamproject9_contact
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.key
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import com.example.teamproject9_contact.databinding.FragmentMyPageBinding
 
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 
 class MyPageFragment : Fragment() {
@@ -28,19 +34,38 @@ class MyPageFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
 
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       binding = FragmentMyPageBinding.inflate(inflater)
+        binding = FragmentMyPageBinding.inflate(inflater)
 
+        setFragmentResultListener("name") { key, bundle ->
+            bundle.getString("name")?.let {
+                binding.textMyPageName.text = bundle.getString("name")
+            }
+        }
 
-//        binding.btnMyPage.setOnClickListener {
-//            floatingButton()
-//        }
+        setFragmentResultListener("number") { key, bundle ->
+            bundle.getString("number")?.let {
+                binding.textMyPagePhoneNumber.text = bundle.getString("number")
+            }
+        }
 
+        setFragmentResultListener("email") { key, bundle ->
+            bundle.getString("email")?.let {
+                binding.textMyPageEmail.text = bundle.getString("email")
+            }
+        }
+
+//        setFragmentResultListener("image") { key, bundle ->
+//            val image = bundle.getString("image")
+//                binding.imgMyPageTitle.setImageURI(bundle.getString("image"))
+//            }
 
 
         return binding.root
@@ -56,10 +81,5 @@ class MyPageFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    private fun floatingButton() {
-        val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+"01012345678"))
-        startActivity(intent)
     }
 }
